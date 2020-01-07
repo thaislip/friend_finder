0362189af609faa2e3abc8f5d7c7e730743bbdce
thaislip <taber.haislip@gmail.com>
2020-01-07 17:15:02 -0500

    survey submit button not working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8180;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
